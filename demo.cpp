1c5315186a40fd9a08f7d37bd1af49a05f9e8029
ysinghshashank <ysinghshashank@gmail.com>
2023-12-09 00:12:06 +0530

    commit 1

---
+++ demo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+        int n;
+        cin>>n;
+        int fib[n];
+        fib[0]=1;
+        fib[1]=1;
+        for(int i=2;i<n;i++){
+                fib[i]=fib[i-1]+fib[i-2];
+        }
+        for(int i=0;i<n;i++){
+                cout<<fib[i]<<" ";
+        }
+}
