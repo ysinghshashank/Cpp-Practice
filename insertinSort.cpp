8bb4b8044d60bad08d0c31bfe01b85e1cee739c8
ysinghshashank <ysinghshashank1998@gmail.com>
2024-03-14 20:46:18 +0530

    14/03/2024

---
+++ insertinSort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void insertionSort(int arr[],int n){
+    for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j = i - 1;
+        while(j >= 0 and key < arr[j]){
+            arr[j+1]=arr[j];
+            j = j - 1;
+        }
+        arr[j+1]=key;
+    }
+};
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+    insertionSort(arr,n);
+    // for(int i = 0;i < n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+}
