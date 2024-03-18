b267027b0bd17dd96f7bc504589d2b7acdaa18bb
ysinghshashank <ysinghshashank1998@gmail.com>
2024-03-18 20:35:09 +0530

    Recursion

--- Recursion.cpp
+++ Recursion.cpp
@@ -8,6 +8,15 @@ if(n==0){
 }
 return sumOfNumber(n-1)+n;
 };
+//1 to N Without Loop (coding ninja)
+vector<int> printNos(int x) {
+    if(x==0)
+        return {};
+    vector <int> ans = printNos(x-1);
+    ans.push_back(x);
+    return ans;
+
+}
 int main(){
 	int n;
 	cin>>n;
