11c77ae08756e1c0890ed9372c1c0bceb9cfa319
ysinghshashank <ysinghshashank1998@gmail.com>
2024-03-22 22:58:13 +0530

    cpp

--- Prime_factor.cpp
+++ Prime_factor.cpp
@@ -4,26 +4,19 @@ using namespace std;
 int main()
 {
     int n = 405;
-    
     vector<int> primes;
-    
     if(n%2 == 0){
-        primes.push_back(2);
-        
+        primes.push_back(2);   
         while(n%2 == 0)
         n /= 2;
     }
-    
     for(int i=3; i*i<=n ; i+=2){
-        
         if(n%i == 0){
             primes.push_back(i);
-            
             while(n%i == 0)
             n/= i;
         }
     }
-    
     if(n > 1)
     primes.push_back(n);
     for(int i=0; i<primes.size(); i++)
